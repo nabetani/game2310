e8a2f4558ea8f8fdc9e6bc853648a33a2e79ae09
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-10-24 08:29:28 +0900

    画像追加など

--- src/gameMain.ts
+++ src/gameMain.ts
@@ -1,17 +1,26 @@
 import * as Settings from './settings';
 
+const earthH = 40;
+
 export class GameMain extends Phaser.Scene {
+  p0: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | null = null
   constructor() {
     super('GameMain');
   }
   preload() {
-    this.load.image('bg2', 'assets/bg2.png');
+    for (const n of ["game_bg", "earth", "p0", "p1"]) {
+      this.load.image(n, `assets/${n}.png`);
+    }
   }
   create() {
-    this.add.image(Settings.bgSize.x / 2, Settings.bgSize.y / 2, 'bg2');
+    this.add.image(Settings.bgSize.x / 2, Settings.bgSize.y / 2, 'game_bg');
+    let staticGroup = this.physics.add.staticGroup();
+    let earth = staticGroup.create(Settings.bgSize.x / 2, Settings.bgSize.y - earthH / 2, 'earth');
     let text = this
       .add
       .text(Settings.bgSize.x / 2, Settings.bgSize.y / 2, 'You are playing...', { fontFamily: 'arial', fontSize: '60px' })
-      .setOrigin(0.5).setInteractive();
+      .setOrigin(0.5);
+    this.p0 = this.physics.add.sprite(240, 80, "p0");
+    this.physics.add.collider(this.p0, staticGroup);
   }
 }
