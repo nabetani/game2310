4b9a8e71867fe529dcdbe77c6b394049027d9da0
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-10-26 00:33:59 +0900

    遊び方

--- src/waiting.ts
+++ src/waiting.ts
@@ -9,16 +9,52 @@ export class Wating extends Phaser.Scene {
     this.load.image('bg', 'assets/bg.png');
   }
 
+  addText(msg: string, x: number, y: number, org: number, fontSize: string): Phaser.GameObjects.Text {
+    const attr = { fontFamily: 'arial', fontSize: fontSize };
+    let text = this.add
+      .text(x, y, msg, attr)
+      .setOrigin(org, 0.5).setInteractive();
+    return text;
+  }
+
   create() {
     this.add.image(Settings.bgSize.x / 2, Settings.bgSize.y / 2, 'bg');
     const msg = 'Click here to start game.';
     const attr = { fontFamily: 'arial', fontSize: '60px' };
-    let text = this.add
-      .text(Settings.bgSize.x / 2, Settings.bgSize.y / 2, msg, attr)
-      .setOrigin(0.5).setInteractive();
-    text.on('pointerdown', () => {
-      console.log("pointer down on text")
+    const startText = this.addText(
+      'Click here to start game.',
+      Settings.bgSize.x / 2, Settings.bgSize.y / 2, 0.5,
+      '60px');
+    startText.on('pointerdown', () => {
       this.scene.start('GameMain');
+    });
+    const showRuleJ = this.addText(
+      '遊び方',
+      50, Settings.bgSize.y * 0.75, 0,
+      '40px');
+    const rules = [
+      "押している間はジャンプ力をためます。\n離すとジャンプ。\n落下中にタイツに触れてください。",
+      "Press to charge the power to jump.\nRelease to jump.\nTouch the tights while falling."
+    ].map(e => {
+      const r = this.addText(
+        e,
+        Settings.bgSize.x / 2, Settings.bgSize.y * 0.2, 0.5,
+        '30px')
+      r.setVisible(false);
+      return r;
+    });
+
+    showRuleJ.on('pointerdown', () => {
+      rules[0].setVisible(true);
+      rules[1].setVisible(false);
+    });
+    const showRuleE = this.addText(
+      'How to play',
+      Settings.bgSize.x - 50, Settings.bgSize.y * 0.75, 1,
+      '40px');
+    showRuleE.on('pointerdown', () => {
+      rules[0].setVisible(false);
+      rules[1].setVisible(true);
     });
   }
   update() {
